b7b03ee1ac52a1a29f36e0d5810ba65623445b9c
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-09-06 17:45:18 +0900

    0906 commit

--- programmers.java
+++ programmers.java
@@ -4,21 +4,16 @@ import java.util.Arrays;
 public class programmers {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String[] my_strings = new String[]{"progressive", "hamburger", "hammer", "ahocorasick"};
-        int[][] parts = new int[][] {{0, 4}, {1, 2}, {3, 5}, {7,7}};
-        solution.solution(my_strings, parts);
+        String[] names = new String[]{"nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"};
+//        int[][] parts = new int[][] {{0, 4}, {1, 2}, {3, 5}, {7,7}};
+        int[] num_list = new int[]{4, 2, 6, 1, 7, 6};
+        solution.solution(num_list,2);
     }
 }
-
 class Solution {
-    public String solution(String[] my_strings, int[][] parts) {
-        String answer = "";
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
 
-        for (int i = 0; i < my_strings.length; i++) {
-            String str = my_strings[i];
-            answer += str.substring(parts[i][0], parts[i][1] + 1);
-        }
-        System.out.println(answer);
         return answer;
     }
 }
